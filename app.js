10e172e6948dc8702cabb4aa5212af124ff6ffef
JaeBeen95 <nin2348@gmail.com>
2024-10-10 16:01:31 +0900

    feat: 각 title에 a태그 적용

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.hnpwa.com/v0/news/1.json", false);
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+
+xhr.open("GET", NEWS_URL, false);
 xhr.send();
 
 const newsFeed = JSON.parse(xhr.response);
@@ -7,9 +9,12 @@ const ul = document.createElement("ul");
 
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
+  const a = document.createElement("a");
 
-  li.innerHTML = newsFeed[i].title;
+  a.href = '#'
+  a.innerHTML = newsFeed[i].title;
 
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
