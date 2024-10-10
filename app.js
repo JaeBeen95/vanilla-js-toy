4b62278b357883d72555b7446b76245a08ea89d2
JaeBeen95 <nin2348@gmail.com>
2024-10-10 17:24:03 +0900

    feat: 라우터 적용

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const xhr = new XMLHttpRequest();
 const container = document.getElementById("root");
-const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
@@ -11,32 +10,51 @@ function getData(url) {
   return JSON.parse(xhr.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
-
-window.addEventListener("hashchange", function () {
+function newsDetail() {
   const id = location.hash.substring(1);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
 
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-const newsList = [];
-newsList.push("<ul>");
-
-for (let i = 0; i < 10; i++) {
-  newsList.push(`
-    <li>
-      <a href='#${newsFeed[i].id}'>
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
-  `);
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    
+    <div>
+      <a href='#'>목록으로</a>
+    </div>
+  `;
 }
 
-newsList.push("</ul>");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-container.innerHTML = newsList.join("");
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
+        <a href='#${newsFeed[i].id}'>
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function router() {
+  const routerPath = location.hash;
+
+  if (routerPath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
