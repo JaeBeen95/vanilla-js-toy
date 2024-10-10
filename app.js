a860863e2252451f14c67825669bb28cce0f717d
JaeBeen95 <nin2348@gmail.com>
2024-10-10 16:23:50 +0900

    feat: 각 뉴스 아이템 타이틀 렌더링

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const xhr = new XMLHttpRequest();
+const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 xhr.open("GET", NEWS_URL, false);
 xhr.send();
@@ -7,15 +9,29 @@ xhr.send();
 const newsFeed = JSON.parse(xhr.response);
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", function () {
+  const id = location.hash.substring(1);
+
+  xhr.open("GET", CONTENT_URL.replace("@id", id), false);
+  xhr.send();
+
+  const newsContent = JSON.parse(xhr.response);
+  const title = document.createElement("h1");
+
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
   const a = document.createElement("a");
 
-  a.href = '#'
-  a.innerHTML = newsFeed[i].title;
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
 
   li.appendChild(a);
   ul.appendChild(li);
 }
 
 document.getElementById("root").appendChild(ul);
+document.getElementById("root").appendChild(content);
