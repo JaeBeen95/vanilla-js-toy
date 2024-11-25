017dd3322742e72bbb61d7316a65d40241577c45
JaeBeen95 <nin2348@gmail.com>
2024-11-25 21:18:22 +0900

    refactor: 모듈화

---
+++ src/core/api.ts
@@ -0,0 +1,30 @@
+import type { NewsDetail, NewsFeed } from "../types";
+
+export class Api {
+  url: string;
+  ajax: XMLHttpRequest;
+
+  constructor(url: string) {
+    this.url = url;
+    this.ajax = new XMLHttpRequest();
+  }
+
+  getRequest<ajaxResponse>(): ajaxResponse {
+    this.ajax.open("GET", this.url, false);
+    this.ajax.send();
+
+    return JSON.parse(this.ajax.response);
+  }
+}
+
+export class NewsFeedApi extends Api {
+  getData(): NewsFeed[] {
+    return this.getRequest<NewsFeed[]>();
+  }
+}
+
+export class NewsDetailApi extends Api {
+  getData(): NewsDetail {
+    return this.getRequest<NewsDetail>();
+  }
+}
---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+import View from "../core/view";
+
+export interface Store {
+  currentPage: number;
+  feeds: NewsFeed[];
+}
+
+export interface News {
+  readonly id: number;
+  readonly user: string;
+  readonly time: number;
+  readonly time_ago: string;
+  readonly type: string;
+  readonly url: string;
+}
+
+export interface NewsFeed extends News {
+  readonly title: string;
+  readonly points: number;
+  readonly comments_count: number;
+  readonly domain: string;
+  read?: boolean;
+}
+
+export interface NewsDetail extends News {
+  readonly title: string;
+  readonly points: number;
+  readonly content: string;
+  readonly comments: NewsComments[];
+  readonly comments_count: number;
+  readonly domain: string;
+}
+
+export interface NewsComments extends News {
+  readonly content: string;
+  readonly comments: NewsComments[];
+  readonly level: number;
+}
+
+export interface RouteInfo {
+  path: string;
+  page: View;
+}
